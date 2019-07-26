14844a03b310bae1d8e3ac4eee59c61de1e16e32
lilieming <53309468+llmgit@users.noreply.github.com>
2019-07-26 23:31:07 +0800

    Add files via upload

---
+++ Source/MyMaterials/MyMaterialsGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyMaterialsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYMATERIALS_API AMyMaterialsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
